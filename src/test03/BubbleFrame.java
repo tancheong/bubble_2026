2f2c4fd32b7ca8f06c25f331ff1e4b2f0bc8ae68
이정민 <protain02@naver.com>
2026-03-12 15:13:26 +0900

    feat: test03 벽 충돌 백그라운드 서비스 구축

---
+++ src/test03/BubbleFrame.java
@@ -0,0 +1,84 @@
+package test03;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+    private JLabel backGroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initData();
+        setInitLayout();
+        addEventListener();
+        // 충돌 감지 백그라운드 서비스 시작
+        new Thread(new BackgroundPlayerService(player)).start();
+    }
+
+    private void initData() {
+        super.setTitle("버블버블 게임");
+        setSize(1000, 640);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        backGroundMap = new JLabel(new ImageIcon("img/backgroundMap.png"));
+        setContentPane(backGroundMap);
+
+        player = new Player();
+    }
+
+    private void setInitLayout() {
+        setLayout(null);
+        setResizable(false); // 창 크기 고정
+        setLocationRelativeTo(null); // 화면 정중앙 배치(프레임)
+
+        backGroundMap.add(player);
+
+        setVisible(true);
+    }
+
+    private void addEventListener() {
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        player.setLeft(false);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.setRight(false);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // 방향키 코드를 Player의 이동 메서드로 연결
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        // 이동 중이 아니고 AND 벽에 충돌하지 않은 상태일 때만 left() 호출 가능
+                        if (player.isLeft() == false && player.isLeftWallCrash() == false) {
+                            player.left();
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        // 이동 중이 아니고 AND 벽에 충돌하지 않은 상태일 때만 right() 호출 가능
+                        if (player.isRight() == false && player.isRightWallCrash() == false) {
+                            player.right();
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.up();
+                        break;
+                }
+            }
+        });
+    }
+
+    // 테스트 코드
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+
+} // end of class
---
+++ src/test03/Moveable.java
@@ -0,0 +1,8 @@
+package test03;
+
+public interface Moveable {
+    void left();
+    void right();
+    void up();
+    void down();
+}
